84579d59d24197406ec21675d149ff2af798845c
Haodong Chen <chen.hdong@gmail.com>
2022-02-10 19:56:17 -0800

    Create Somatic_ctDNA_without_UMI.sh

---
+++ example_pipelines/somatic/TNscope/Somatic_ctDNA_without_UMI.sh
@@ -0,0 +1,118 @@
+#!/bin/sh
+
+# Copyright (c) 2016-2020 Sentieon Inc. All rights reserved
+
+# *******************************************
+# Script to perform TNscope variant calling on
+# hybridization captured cfDNA tumor sample  
+# without UMI.
+# *******************************************
+
+set -eu
+
+# Update with the fullpath location of your sample fastq
+TUMOR_SM="tumor_sample" #sample name
+TUMOR_RGID="rg_$TUMOR_SM" #read group ID
+PL="ILLUMINA" #or other sequencing platform
+FASTQ_FOLDER="/home/pipeline/samples"
+TUMOR_FASTQ_1="$FASTQ_FOLDER/tumor_1.fastq.gz"
+TUMOR_FASTQ_2="$FASTQ_FOLDER/tumor_2.fastq.gz" #If using Illumina paired data
+
+# Update with the location of the reference data files
+FASTA_DIR="/home/regression/references/b37"
+FASTA="$FASTA_DIR/human_g1k_v37_decoy.fasta"
+KNOWN_DBSNP="$FASTA_DIR/dbsnp_138.b37.vcf.gz"
+INTERVAL_FILE="targeted_regions.bed"
+
+# Update with the location of the Sentieon software package and license file
+SENTIEON_INSTALL_DIR=/home/release/sentieon-genomics-|release_version|
+TNSCOPE_FILTER=/home/sentieon-scripts/tnscope_filter/tnscope_filter.py
+export SENTIEON_LICENSE=/home/Licenses/Sentieon.lic #or using licsrvr: c1n11.sentieon.com:5443
+
+# Other settings
+NT=$(nproc) #number of threads to use in computation, set to number of cores in the server
+START_DIR="$PWD/test/TNscope" #Determine where the output files will be stored
+
+# ************************************************************************************************************************************************************************
+
+# ******************************************
+# 0. Setup
+# ******************************************
+WORKDIR="$START_DIR"
+mkdir -p $WORKDIR
+LOGFILE=$WORKDIR/run.log
+exec >$LOGFILE 2>&1
+cd $WORKDIR
+
+# ******************************************
+# 1. Mapping reads with BWA-MEM, sorting for the tumor sample
+# ******************************************
+#The results of this call are dependent on the number of threads used. To have number of threads independent results, add chunk size option -K 10000000 
+( $SENTIEON_INSTALL_DIR/bin/sentieon bwa mem -R "@RG\tID:$TUMOR_RGID\tSM:$TUMOR_SM\tPL:$PL" \
+    -t $NT -K 10000000 $FASTA $TUMOR_FASTQ_1 $TUMOR_FASTQ_2 || \
+    { echo -n 'BWA error'; exit 1; } ) | \
+    $SENTIEON_INSTALL_DIR/bin/sentieon util sort -o tumor_sorted.bam -t $NT --sam2bam -i - || \
+    { echo "Alignment failed"; exit 1; }
+
+# ******************************************
+# 2. Metrics for the tumor sample
+# ******************************************
+$SENTIEON_INSTALL_DIR/bin/sentieon driver -r $FASTA -t $NT -i tumor_sorted.bam \
+    --algo MeanQualityByCycle tumor_mq_metrics.txt \
+    --algo QualDistribution tumor_qd_metrics.txt --algo GCBias \
+    --summary tumor_gc_summary.txt tumor_gc_metrics.txt --algo AlignmentStat \
+    --adapter_seq '' tumor_aln_metrics.txt --algo InsertSizeMetricAlgo tumor_is_metrics.txt || \
+    { echo "Metrics failed"; exit 1; }
+
+$SENTIEON_INSTALL_DIR/bin/sentieon plot GCBias -o tumor_gc-report.pdf tumor_gc_metrics.txt
+$SENTIEON_INSTALL_DIR/bin/sentieon plot QualDistribution \
+    -o tumor_qd-report.pdf tumor_qd_metrics.txt
+$SENTIEON_INSTALL_DIR/bin/sentieon plot MeanQualityByCycle \
+    -o tumor_mq-report.pdf tumor_mq_metrics.txt
+$SENTIEON_INSTALL_DIR/bin/sentieon plot InsertSizeMetricAlgo \
+    -o tumor_is-report.pdf tumor_is_metrics.txt
+
+# ******************************************
+# 3. Remove duplicate reads for the tumor sample. 
+# It is possible to remove instead of mark 
+# duplicates by adding the --rmdup option in Dedup
+# ******************************************
+$SENTIEON_INSTALL_DIR/bin/sentieon driver -t $NT -i tumor_sorted.bam --algo LocusCollector \
+    --fun score_info tumor_score.txt || { echo "LocusCollector failed"; exit 1; }
+
+$SENTIEON_INSTALL_DIR/bin/sentieon driver -t $NT -i tumor_sorted.bam --algo Dedup \
+    --score_info tumor_score.txt --metrics tumor_dedup_metrics.txt tumor_deduped.bam || \
+    { echo "Dedup failed"; exit 1; }
+
+# ******************************************
+# 4. Coverage metrics for the tumor sample
+# ******************************************
+$SENTIEON_INSTALL_DIR/bin/sentieon driver -r $FASTA -t $NT ${INTERVAL_FILE:+--interval $INTERVAL_FILE} -i tumor_deduped.bam \
+    --algo CoverageMetrics tumor_coverage_metrics || { echo "CoverageMetrics failed"; exit 1; }
+
+# ******************************************
+# 5. Somatic and Structural variant calling
+# ******************************************
+$SENTIEON_INSTALL_DIR/bin/sentieon driver -r $FASTA -t $NT \
+    ${INTERVAL_FILE:+--interval $INTERVAL_FILE} \
+    -i tumor_deduped.bam \
+    --algo TNscope \
+    --tumor_sample $TUMOR_SM \
+    --dbsnp $KNOWN_DBSNP \
+    --disable_detector sv \
+    --min_tumor_allele_frac 3e-3 \
+    --min_tumor_lod 3.0 \
+    --min_init_tumor_lod 1.0 \
+    --assemble_mode 2 \
+    output_tnscope.pre_filter.vcf.gz || \
+    { echo "TNscope failed"; exit 1; }
+
+# ******************************************
+# 6. Variant filtration
+# ******************************************
+$SENTIEON_INSTALL_DIR/bin/sentieon pyexec $TNSCOPE_FILTER \
+    -v output_tnscope.pre_filter.vcf.gz \
+    --tumor_sample $TUMOR_SM \
+    -x ctdna --min_tumor_af 0.005 --min_depth 400 \
+    output_tnscope.filter.vcf.gz || \
+    { echo "TNscope filter failed"; exit 1; }
